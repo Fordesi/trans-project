fd595fc907ed9ac99e50e2948eed820b74151d2e
unknown <withunyuu@gmail.com>
2024-05-04 21:12:10 +0700

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'demo';
+  input = '';
+  changeName(){
+    this.title = this.input;
+  }
+  
+}
+
